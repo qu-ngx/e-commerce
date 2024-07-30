d3cc522edaed20961fc3914c4ac732d58ec4133d
qu-ngx <quang13012005@gmail.com>
2024-07-30 13:48:21 -0700

    init controller and service for product

---
+++ services/product/src/main/java/com/qungx/ecommerce/product/ProductController.java
@@ -0,0 +1,13 @@
+package com.qungx.ecommerce.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService service;
+
+}
