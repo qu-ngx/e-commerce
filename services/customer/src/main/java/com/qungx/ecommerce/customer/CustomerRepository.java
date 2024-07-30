ca8007884de0b99e0d34c95d814159c69d4011e8
qu-ngx <quang13012005@gmail.com>
2024-07-30 10:24:08 -0700

    added handlers for product service

--- services/customer/src/main/java/com/qungx/ecommerce/customer/CustomerRepository.java
+++ services/customer/src/main/java/com/qungx/ecommerce/customer/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.qungx.ecommerce.customer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
